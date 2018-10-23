55e3c49394b05339c37549986320a30803a7e6b3
Deivy <dmendoza@utch.edu.mx>
2018-10-23 13:28:49 -0600

    Usuario y token listo

---
+++ routes/app.js
@@ -0,0 +1,14 @@
+var express = require('express');
+
+var app = express();
+
+
+app.get('/', (req, res, next) => {
+
+    res.status(403).json({
+        ok: true,
+        mensaje: 'Peticion realizada correctamente'
+    })
+});
+
+module.exports = app;
