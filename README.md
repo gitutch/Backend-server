eb69beb9abaa1662458cc7a616debce13f062f2d
Deivy <dmendoza@utch.edu.mx>
2018-10-26 12:19:28 -0600

    backend terminado

---
+++ README.md
@@ -0,0 +1,4 @@
+# Backend-Server
+
+Este es el codigo necesario para establecer el backend
+conectado a MongoDB usando Mongoose.
--- config/config.js
+++ config/config.js
@@ -1,1 +1,5 @@
 module.exports.SEED = '@este-es@-un-seed-@dificil';
+
+
+// Google
+module.exports.CLIENT_ID = '1055119431166-cv36kragn1qvnqf1bnqjfbr5lfrm2lhm.apps.googleusercontent.com';
